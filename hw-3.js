85f3924931f5b4b74aa2cd29abff3a57d0d90a2f
92Ezio48 <vovanew77714@gmail.com>
2024-12-04 22:35:05 +0300

    created task#5 hw-3

--- hw-3.js
+++ hw-3.js
@@ -28,3 +28,22 @@ if (d > 100 || e > 100) {
 let a = '2';
 let b = '3';
 alert(+a + +b);
+
+
+let month = prompt("Введите номер месяца");
+switch (month) {
+    case '1' || '2' || '12':
+        alert('Зима');
+        break;
+    case '3' || '4' || '5':
+        alert('Весна');
+        break;
+    case '6' || '7' || '8':
+        alert('Лето');
+        break;
+    case '9' || '10' || '11':
+        alert('Осень');
+        break;
+    default:
+        alert('Ввели что-то не то');
+}
