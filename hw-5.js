e1afa13909287ebecef89e435bca054727140102
92Ezio48 <vovanew77714@gmail.com>
2024-12-12 23:19:47 +0300

    created task#4 hw-5

--- hw-5.js
+++ hw-5.js
@@ -9,6 +9,7 @@ console.log(min(4, 7));
 console.log(min(8, 2));
 console.log(min(6, 6));
 
+
 function parity(parityNumber) {
     if (parityNumber % 2 == 0) {
 		return console.log('Число четное');
@@ -27,3 +28,17 @@ function square(NumberEntered) {
 }
 console.log(square(8));
 console.log(square(12));
+
+
+function howOld() {
+    let age = prompt("Сколько тебе лет?");
+    if (age < 0) {
+        alert('Вы ввели неправильное значение');
+    } else if (age >= 0 && age <= 12) {
+        alert('Привет, друг!');
+    }else {
+        alert('Добро пожаловать!');
+    }
+}
+console.log(howOld());
+
