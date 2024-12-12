6b37b8ae46c9e7439925ced7faec548c5ed81ed2
92Ezio48 <vovanew77714@gmail.com>
2024-12-12 22:59:45 +0300

    created task#2 hw-5

--- hw-5.js
+++ hw-5.js
@@ -8,3 +8,17 @@ function min(a, b) {
 console.log(min(4, 7));
 console.log(min(8, 2));
 console.log(min(6, 6));
+
+function parity(parityNumber) {
+    if (parityNumber % 2 == 0) {
+		return console.log('Число четное');
+} else {
+	return console.log('Число нечетное');
+	}
+}
+console.log(parity(1));
+console.log(parity(2));
+console.log(parity(3));
+
+
+
