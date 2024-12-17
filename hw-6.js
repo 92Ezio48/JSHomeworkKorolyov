42f84ca5434a9122c51229ebe9632a865ecc64df
92Ezio48 <vovanew77714@gmail.com>
2024-12-17 21:01:28 +0300

    created task #3 hw-6

--- hw-6.js
+++ hw-6.js
@@ -1,10 +1,16 @@
-const secondSet = [1, 5, 4, 10, 0, 3];
-const secondGoal = secondSet.indexOf(4);
-console.log(secondGoal);
-
-
 const firstSet = [1, 5, 4, 10, 0, 3];
 for (let i = 0; i < firstSet.length; i++) {
     if (firstSet[i] == 0) break;
     console.log(firstSet[i]);
  }
+
+
+const secondSet = [1, 5, 4, 10, 0, 3];
+const secondGoal = secondSet.indexOf(4);
+console.log(secondGoal);
+
+
+const thirdSet = [1, 3, 5, 10, 20];
+const thirdGoal = thirdSet.join(' ');
+console.log(thirdGoal);
+
