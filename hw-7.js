73e3b0ec923fffe9da625b6518434b9da2a787bc
92Ezio48 <vovanew77714@gmail.com>
2025-01-06 14:07:42 +0300

    created task #2 hw-7

--- hw-7.js
+++ hw-7.js
@@ -4,3 +4,15 @@ console.log(firstExercise.toUpperCase());
 
 
 // 2
+function secondFunction(array, string){
+    const filtered = array.filter(age => {
+        if (age.toLowerCase().startsWith(string.toLowerCase())){
+            return age;  
+        }
+});
+return filtered; 
+}
+let result = secondFunction(['Фланец', 'Обечайка', 'Корпус верхний', 'Корпус нижний' ], 'корпус');
+console.log(result);
+
+// 3
